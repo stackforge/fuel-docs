70b4a31dfb4fdebd596208793a1a7da7ebe5b8bc
Meg McRoberts <mmcroberts@mirantis.com>
2014-04-22 23:29:05 -0700

    Moved information from install to separate vbox doc
    I did a wee bit of wordsmithing but the content is unchanged.
    
    Change-Id: I1d8e10f7c2ef37069d1c55c7b2b18368b39a8c31

--- pdf/conf.py
+++ pdf/conf.py
@@ -21,6 +21,7 @@ pdf_documents = [
     u'2014, Mirantis Inc.'),
     ('pdf/pdf_install', u'Mirantis-OpenStack-4.1-InstallGuide', u'Installation Guide', u'2014, Mirantis Inc.'),
     ('pdf/pdf_operations', u'Mirantis-OpenStack-4.1-OperationsGuide', u'Operations Guide', u'2014, Mirantis Inc.'),
+   ('pdf/pdf_virtualbox', u'Running-Mirantis-OpenStack-on-VirtualBox', u'Running Fuel on VirtualBox', u'2014, Mirantis Inc.'),
     ('pdf/pdf_reference', u'Mirantis-OpenStack-4.1-ReferenceArchitecture', u'Reference Architecture', u'2014, Mirantis Inc.'),
     ('pdf/pdf_preinstall', u'Mirantis-OpenStack-4.1-Pre-InstallationGuide', u'Pre-Installation Guide', u'2014, Mirantis Inc.'),
     ('pdf/pdf_terminology', u'Mirantis-OpenStack-4.1-Terminology-Reference', u'Terminology Reference', u'2014, Mirantis Inc.'),
