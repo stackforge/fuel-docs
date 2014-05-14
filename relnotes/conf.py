c91c7bc2c1a95b6150ad8e0d618922a18a4ef131
Meg McRoberts <mmcroberts@mirantis.com>
2014-05-14 01:39:00 -0700

    Update control files to 5.0 and date to May 2014
    List of major releases is removed from preface.rst: the intention of this table
    is to track revisions of documentation for the current major release.
    
    Change-Id: If0d87ccfdcb9b312174a6e1ddeefa03dd38f253f

--- relnotes/conf.py
+++ relnotes/conf.py
@@ -281,8 +281,7 @@ nwdiag_antialias = True
 
 extensions += ['rst2pdf.pdfbuilder']
 pdf_documents = [ 
-    ('relnotes/index', u'Mirantis-OpenStack-4.1-RelNotes',  u'Release Notes',
-    u'2014, Mirantis Inc.')
+    ('relnotes/index', u'Mirantis-OpenStack-5.0-RelNotes',  u'Release Notes', u'2014, Mirantis Inc.')
 #    (master_doc, project, project, copyright),
 ]
 pdf_stylesheets = ['letter', 'mirantis']
