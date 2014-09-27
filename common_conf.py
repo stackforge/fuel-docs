f958ca28eb1ea84d1b24d49cb7b57d907064119a
Christopher Aedo <doc@aedo.net>
2014-09-26 17:40:09 -0700

    Clone fuel-web repo when building docs
    This commit clones the fuel-web repo when
    "make html" and other doc-building commands
    are issued. This is neccessary in order to
    include automatically generated Nailgun API
    docs in Fuel documentation.
    
    Implements: blueprint fuel-dev-docs-merge-fuel-docs
    Closes-bug #1374239
    
    Change-Id: Ife69f4ac047bd98b1840eaf78a9ab876a2ef0249

--- common_conf.py
+++ common_conf.py
@@ -14,8 +14,6 @@
 import sys, os
 import cloud_sptheme as csp
 
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
