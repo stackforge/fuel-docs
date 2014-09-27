a4e5da1b7b63e945d49a57c93b0fd5dc2942d164
Christopher Aedo <doc@aedo.net>
2014-09-26 17:40:09 -0700

    Clone fuel-web repo when building docs
    This commit clones the fuel-web repo when
    "make html" and other doc-building commands
    are issued. This is neccessary in order to
    include automatically generated Nailgun API
    docs in Fuel documentation.
    
    Implements: blueprint fuel-dev-docs-merge-fuel-docs
    Closes-bug #1374239
    
    Change-Id: Ife69f4ac047bd98b1840eaf78a9ab876a2ef0249

---
+++ repo.mk
@@ -0,0 +1,4 @@
+# Specify git clone URL for fuel-web repository here
+# master branch is assumed unless otherwise specified
+NAILGUN_REPO=https://github.com/stackforge/fuel-web.git
+NAILGUN_COMMIT=master
